31921dd056074cad47ea376d709b4d3516d75e04
Mitchell <120224887+Mitchell-Medeiros@users.noreply.github.com>
2024-07-11 11:25:08 +0300

    Refactored Dataset classes and improved several docstrings

--- src/data/dataset_validation.py
+++ src/data/dataset_validation.py
@@ -1,94 +1,170 @@
 import json
 import logging
 from pathlib import Path
-from typing import Any
+from typing import Any, Literal, TypeAlias
 
 from pydantic import BaseModel
 
+from .dataset_processor import DatasetType
+
 logger = logging.getLogger(__name__)
 
+Parameters: TypeAlias = dict[str, Any]
+ParametersList: TypeAlias = list[Parameters]
 
-class KaggleDataset(BaseModel):
+
+class BaseDataset(BaseModel):
+    """
+    Base class for datasets, containing attributes related to data cleaning and reformatting.
+
+    Attributes:
+    -----------
+        dataset_type (Literal["kaggle", "local"]): Type of dataset. Accepts 'kaggle' or 'local'.
+        target_column (str): Column name for the target of the predictions.
+        columns_to_drop (list[str] | None): List of column names to be dropped from the dataset.
+        columns_to_encode (list[str] | None): List of column names to be one-hot encoded in the dataset.
+    """
+
+    dataset_type: Literal["kaggle", "local"]
+    target_column: str
+    columns_to_drop: list[str] | None
+    columns_to_onehot_encode: list[str] | None
+
+
+class KaggleDataset(BaseDataset):
     """
     Represents a Kaggle dataset with all the fields required to download and prepare it for model evaluation.
 
-    ### Attributes:
-        username (str): The username of the Kaggle user who owns the dataset.
-        dataset_name (str): The name of the Kaggle dataset.
-        file_name (str): The name of the file associated with the dataset.
-        save_name (str): The name to use for files saved from this dataset. Should be unique across datasets.
-        target_column (str): The name of the target column in the dataset.
-        columns_to_drop (list[str] | None): A list of column names to be dropped from the dataset.
-    If None, no columns will be dropped.
-        columns_to_encode (list[str] | None): A list of column names to be encoded in the dataset.
-    If None, no columns will be encoded.
+    Attributes:
+    -----------
+        dataset_type (Literal["kaggle", "local"]): Type of dataset. Accepts 'kaggle' or 'local'.
+        username (str): Username of the Kaggle user who owns the dataset.
+        dataset_name (str): Name of the Kaggle dataset.
+        file_name (str): Name of the file to be downloaded from the dataset.
+        target_column (str): Name of the target column in the dataset for training and prediction.
+        columns_to_drop (list[str] | None): List of column names to be dropped from the dataset.
+        If None, no columns will be dropped.
+        columns_to_onehot_encode (list[str] | None): List of column names to be encoded in the dataset.
+        If None, no columns will be encoded.
     """
 
     username: str
     dataset_name: str
     file_name: str
-    target_column: str
-    columns_to_drop: list[str] | None
-    columns_to_encode: list[str] | None
 
 
-class LocalDataset(BaseModel):
+class LocalDataset(BaseDataset):
     """
     Represents a locally saved dataset with all the fields required to load and prepare it for model evaluation.
 
-    ### Attributes:
+    Attributes:
+    -----------
+        dataset_type (Literal["kaggle", "local"]): Type of dataset. Accepts 'kaggle' or 'local'.
         file_path (Path): The path to the local dataset file.
         save_name (str): The name to use for files saved from this dataset. Should be unique across datasets.
-        target_column (str): The name of the target column in the dataset.
+        If None, the file will be saved with the same name as the original file.
+        target_column (str): The name of the target column in the dataset for training and prediction.
         columns_to_drop (list[str] | None): A list of column names to be dropped from the dataset.
-    If None, no columns will be dropped.
-        columns_to_encode (list[str] | None): A list of column names to be encoded in the dataset.
-    If None, no columns will be encoded.
-        save_name (str): The name to use for files saved from this dataset. Should be unique across datasets.
-    If None, the file will be saved with the same name as the original file.
+        If None, no columns will be dropped.
+        columns_to_onehot_encode (list[str] | None): A list of column names to be encoded in the dataset.
+        If None, no columns will be encoded.
     """
 
     file_path: Path
-    target_column: str
-    columns_to_drop: list[str] | None
-    columns_to_encode: list[str] | None
     save_name: str | None
 
 
-def validate_dataset_params(
-    dataset_params: list[dict[str, Any]] | Path
-) -> list[KaggleDataset | LocalDataset]:
+def read_from_json_file(file_path: Path) -> ParametersList:
     """
-    Creates a list of KaggleDataset and LocalDataset objects from a dictionary of parameters. You can
-    provide the dictionary directly or a path to a .json file. The outer dictionary should have the
-    dataset name as the key and a dictionary of parameters as the value for each dataset.
+    Read from a JSON file.
 
-    This list returned by this function can be iterated over with the DataProcessor class to process each dataset.
+    Args:
+    -----
+        file_path (Path): Path to the JSON file containing dataset parameters.
 
-    ### Args:
-        The accessed keys are described below.
+    Returns:
+    --------
+        list[dict[str, Any]]: List of dataset parameters.
 
+    Raises:
+    -------
+        FileNotFoundError: If the file is not found at the provided path.
+    """
+    try:
+        with open(file_path, "r") as file:
+            return json.load(file)
+    except FileNotFoundError as e:
+        logger.exception(f"Could not find file: {file_path}")
+        raise e
+
+
+def create_dataset_instance(params: Parameters) -> DatasetType:
+    """
+    Creates an instance of KaggleDataset or LocalDataset based on the value of `dataset_type`
+    within the provided parameters dict.
+
+    Args:
+    -----
+        params (dict[str, Any]): Dictionary of dataset parameters.
+
+    Returns:
+    --------
+        KaggleDataset | LocalDataset: An instance of KaggleDataset or LocalDataset.
+
+    Raises:
+    -------
+        ValueError: If the value of `dataset_type` is not 'kaggle' or 'local'.
+    """
+    dataset_type = params.get("dataset_type")
+    if dataset_type == "kaggle":
+        return KaggleDataset(**params)
+    elif dataset_type == "local":
+        return LocalDataset(**params)
+    else:
+        raise ValueError("dataset_type must be either 'kaggle' or 'local'")
+
+
+def validate_dataset_params(dataset_params: ParametersList | Path) -> list[DatasetType]:
+    """
+    Creates a list of KaggleDataset and LocalDataset objects from a dictionary of parameters.
+
+    You can provide the dictionary directly or a path to a .json file. The list returned by
+    this function can be iterated over with the DataProcessor class to process each dataset.
+
+    Args:
+    -----
         Required keys for all dataset types:
-            dataset_type: The type of dataset. Accepts 'kaggle' or 'local'.
-            target_column: The name of the target column in the dataset.
+            dataset_type Literal["kaggle", "local"]: The type of dataset. Accepts 'kaggle' or 'local'.
+            target_column (str): The name of the target column in the dataset.
 
         Additional required keys for 'kaggle' datasets:
-            username: The Kaggle username of the dataset owner.
-            dataset_name: The name of the Kaggle dataset.
-            file_name: The name of the file to download from the dataset.
+            username (str): The Kaggle username of the dataset owner.
+            dataset_name (str): The name of the Kaggle dataset.
+            file_name (str): The name of the file to download from the dataset.
 
         Additional required keys for 'local' datasets:
-            file_path: The path to the local dataset file. Should be a Path object. The file
+            file_path (str): The path to the local dataset file. Should be a Path object. The file
             should be either within the data directory.
 
         Optional Keys:
-            columns_to_drop: A list of column names to drop from the downloaded data.
-            columns_to_encode: A list of column names to encode using a specific encoding method.
+            columns_to_drop (list[str]): A list of column names to drop from the downloaded data.
+            columns_to_onehot_encode (list[str]): A list of column names to encode using a specific encoding method.
+            save_name (str): The name to use for files saved from this dataset. Should be unique across datasets.
 
-    ### Example Usage:
+    Returns:
+    --------
+        list[DatasetType]: A list of DatasetType objects.
+
+    Raises:
+    -------
+        AssertionError: If the dataset_params are not a list of dictionaries or a path to a .json file.
+        AssertionError: If any list element in `dataset_params` is not a dictionary.
+
+    Example Usage:
+    --------------
     ```python
-    dataset_params = {
-        "dataset1": {
+    dataset_params = [
+        {
             "dataset_type": "kaggle",
             "username": "nomadic-human",
             "dataset_name": "food-prices-by-country",
@@ -97,32 +173,27 @@ def validate_dataset_params(
             "columns_to_drop": ["date", "continent"],
             "columns_to_encode": ["country"]
         },
-        "dataset2": {
-            "dataset_type": "local",
-            "file_path": a_path_object,
-            "target_column": "example",
-            "columns_to_drop": ["column1", "column2"],
-            "columns_to_encode": ["column3", "column4"]
-        },
-        "dataset3": {
+        {
             "dataset_type": "kaggle",
             "username": "dino-lover123",
             "dataset_name": "dinosaur-physiology",
             "file_name": "species.csv",
             "target_column": "skull_circumference",
+        },
+        {
+            "dataset_type": "local",
+            "file_path": a_path_object,
+            "target_column": "example",
+            "columns_to_drop": ["column1", "column2"],
+            "columns_to_encode": ["column3", "column4"]
         }
-    }
+    ]
 
     datasets = validate_datasets(dataset_params)
     ```
     """
     if isinstance(dataset_params, Path):
-        try:
-            with open(dataset_params, "r") as file:
-                dataset_params = json.load(file)
-        except FileNotFoundError as e:
-            logger.exception(f"Could not find file: {dataset_params}")
-            raise e
+        dataset_params = read_from_json_file(dataset_params)
 
     assert isinstance(
         dataset_params, list
@@ -132,22 +203,5 @@ def validate_dataset_params(
         isinstance(params, dict) for params in dataset_params
     ), "Each list element in `dataset_params` must be a dictionary."
 
-    dataset_validators = [
-        (
-            KaggleDataset(**params)
-            if params.get("dataset_type") == "kaggle"
-            else (
-                LocalDataset(**params)
-                if params.get("dataset_type") == "local"
-                else None
-            )
-        )
-        for params in dataset_params
-    ]
-
-    if None in dataset_validators:
-        raise ValueError(
-            "Invalid value for dataset_type for one or more entries. Must be 'kaggle' or 'local'."
-        )
-
-    return dataset_validators  # type: ignore
+    dataset_validators = [create_dataset_instance(params) for params in dataset_params]
+    return dataset_validators
