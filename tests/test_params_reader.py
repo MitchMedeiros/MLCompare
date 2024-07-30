0f1b5ebe0a5b847452975289c16c6731e687b901
Mitchell <120224887+Mitchell-Medeiros@users.noreply.github.com>
2024-07-31 01:42:29 +0300

    Implemented temporary file creation and deletion for some unit tests.

--- tests/test_params_reader.py
+++ tests/test_params_reader.py
@@ -10,13 +10,23 @@ logger = logging.getLogger("mlcompare.params_reader")
 
 
 class TestParamsReader:
-    valid_str_path = "dataset_params.json"
-    valid_path = Path(valid_str_path)
     valid_list = [
         {"param1": "value1"},
         {"param2": "value2"},
     ]
-    invalid_json_path = "invalid_dataset_params.json"
+    invalid_json_content = {"param1": "value1"}
+
+    @pytest.fixture
+    def valid_str_path(self, tmpdir):
+        file_path = tmpdir.join("dataset_params.json")
+        file_path.write(json.dumps(self.valid_list))
+        return str(file_path)
+
+    @pytest.fixture
+    def invalid_json_path(self, tmpdir):
+        file_path = tmpdir.join("invalid_dataset_params.json")
+        file_path.write(json.dumps(self.invalid_json_content))
+        return str(file_path)
 
     def test_valid_list(self):
         result = ParamsReader.read(self.valid_list)
@@ -35,22 +45,24 @@ class TestParamsReader:
         with pytest.raises(AssertionError):
             ParamsReader.read(invalid_input)
 
-    def test_valid_str_path(self):
-        result = ParamsReader.read(self.valid_str_path)
+    def test_valid_str_path(self, valid_str_path):
+        result = ParamsReader.read(valid_str_path)
         assert result == self.valid_list
 
-    def test_valid_path(self):
-        result = ParamsReader.read(self.valid_path)
+    def test_valid_path(self, valid_str_path):
+        result = ParamsReader.read(Path(valid_str_path))
         assert result == self.valid_list
 
     def test_invalid_str_path(self):
         with pytest.raises(FileNotFoundError):
             ParamsReader.read("an_invalid_path.json")
 
-    def test_invalid_data_inside_json_file(self):
+    def test_invalid_data_inside_json_file(self, invalid_json_path):
         with pytest.raises(AssertionError):
-            ParamsReader.read(self.invalid_json_path)
+            ParamsReader.read(invalid_json_path)
 
-    def test_non_json_file(self):
+    def test_non_json_file(self, tmpdir):
+        non_json_path = tmpdir.join("dataset.csv")
+        non_json_path.write("param1,param2\nvalue1,value2")
         with pytest.raises(json.JSONDecodeError):
-            ParamsReader.read("local_dataset.csv")
+            ParamsReader.read(str(non_json_path))
