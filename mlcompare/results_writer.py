3263610c4fbdc0fa790e608c637e36b3b31b4745
Mitchell <120224887+Mitchell-Medeiros@users.noreply.github.com>
2024-10-11 18:53:24 +0200

    Simplified methods in ResultsWriter by adding directory creation to the init. Changed the default naming of directories. Updated methods to be explicitely static.

--- mlcompare/results_writer.py
+++ mlcompare/results_writer.py
@@ -10,18 +10,25 @@ logger = logging.getLogger(__name__)
 
 class ResultsWriter:
     def __init__(self, directory_name: str | Path | None = None) -> None:
-        if isinstance(directory_name, str):
+        if isinstance(directory_name, type(None)):
+            directory_name = self.generate_default_directory()
+        elif isinstance(directory_name, str):
             directory_name = Path(directory_name)
-        elif isinstance(directory_name, (Path, type(None))):
-            pass
-        else:
+        elif not isinstance(directory_name, Path):
             raise TypeError("save_directory must be a string or Path object.")
 
         self.directory_name = directory_name
 
-    def increment_name(self, name: str | Path) -> Path:
+    @staticmethod
+    def generate_default_directory() -> Path:
+        current_datetime = datetime.now().strftime("%y-%m-%dT%H-%M-%S-%f")[:-4]
+        default_name = Path(f"mlc-{current_datetime}")
+        return default_name
+
+    @staticmethod
+    def increment_name(name: str | Path) -> Path:
         """
-        Increments the name the final Path component for a file or directory until it is unique.
+        Increments the final Path component for a file or directory until it is unique.
 
         Args:
         -----
@@ -33,7 +40,6 @@ class ResultsWriter:
         """
         if isinstance(name, str):
             name = Path(name)
-
         if not isinstance(name, Path):
             raise TypeError("name must be a string or Path object.")
 
@@ -44,17 +50,9 @@ class ResultsWriter:
         return name
 
     def clear_model_results(self) -> None:
-        if self.directory_name is not None:
-            model_results = self.directory_name / "model_results.json"
-            if model_results.exists():
-                model_results.unlink()
-        else:
-            raise ValueError("A directory name has not been set.")
-
-    def generate_default_directory(self) -> Path:
-        current_datetime = datetime.now().strftime("%Y-%m-%dT%H-%M-%S-%f")[:-3]
-        default_name = Path(f"mlcompare-results-{current_datetime}")
-        return default_name
+        model_results = self.directory_name / "model_results.json"
+        if model_results.exists():
+            model_results.unlink()
 
     def create_directory(self, overwrite: bool = False) -> Path:
         """
@@ -69,9 +67,6 @@ class ResultsWriter:
             Path: Path to the directory created.
 
         """
-        if self.directory_name is None:
-            self.directory_name = self.generate_default_directory()
-
         if overwrite is False:
             incremented_directory = self.increment_name(self.directory_name)
             incremented_directory.mkdir()
